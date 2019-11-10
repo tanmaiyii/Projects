56e42e3348c9223751faecf866098a07e43e5ece
Tanmaiyii Rao <34455315+tanmaiyii@users.noreply.github.com>
2019-11-10 23:27:43 +0000

    Delete movielensfcn.py

--- Big Data Processing/movielensfcn.py
+++
@@ -1,9 +0,0 @@
-def removeDuplicates((userid, ratings)): #movies the user has rated 
-    (item1, value1) = ratings[0]
-    (item2, value2) = ratings[1]
-    return item1 < item2
-
-def itemItem((item,values)):
-    (itemA, valueA) = values[0]
-    (itemB, valueB) = values[1]
-    return ((itemA, itemB), (valueA, valueB))
